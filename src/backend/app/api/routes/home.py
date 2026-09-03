664095d26c233fc3a12864856b3b4c591f1eb819
bayesianHimanshu <bayesianHimanshu@proton.me>
2026-09-04 01:54:53 +0530

    add logging with loguru

--- src/backend/app/api/routes/home.py
+++ src/backend/app/api/routes/home.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from app.core.logging import get_logger
+
+logger = get_logger()
 
 router = APIRouter(
     prefix="/home",
@@ -6,4 +9,9 @@ router = APIRouter(
 
 @router.get("/")
 def home():
+    logger.info("Home endpoint accessed")
+    logger.debug("Home endpoint accessed")
+    logger.error("Home endpoint accessed")
+    logger.warning("Home endpoint accessed")
+    logger.critical("Home endpoint accessed")
     return {"message": "Welcome to the Modern Fraud Detection API!"}
--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .api.main import api_router
-from .api.core.config import settings
+from .core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
