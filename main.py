920a2302e81713bd0bbf0b8877e405be46ede737
bayesianHimanshu <bayesianHimanshu@proton.me>
2026-09-06 13:02:02 +0530

    feat: initialize postgres db

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from bank-fraud-detection-mlops!")
-
-
-if __name__ == "__main__":
-    main()
--- src/backend/app/core/config.py
+++ src/backend/app/core/config.py
@@ -1,17 +1,24 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
 from typing import Literal
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# config.py -> core -> app -> backend -> src -> repo root
+BASE_DIR = Path(__file__).resolve().parents[4]
 
 class Settings(BaseSettings):
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
+    API_V1_STR: str
+    PROJECT_NAME: str
+    PROJECT_DESCRIPTION: str
+    SITE_NAME: str
+    DATABASE_URL: str = ""
+
     model_config = SettingsConfigDict(
-        env_file="../../.envs/.env.local", 
+        env_file=BASE_DIR / ".envs" / ".env.local",
         env_ignore_empty=True,
         extra="ignore",
     )
-    API_V1_STR: str = ""
-    PROJECT_NAME: str = ""
-    PROJECT_DESCRIPTION: str = ""
-    SITE_NAME: str = ""
 
 settings = Settings()
